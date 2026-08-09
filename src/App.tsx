8055cdd4979cb9b23208b637dd860fa90ad87f5a
jagdish mehra <jagdishsinghmehra25@gmail.com>
2026-08-09 17:39:37 +0530

    fix email deploy issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,42 @@
-import { useEffect } from 'react'
-import { RouterProvider } from 'react-router-dom'
-import { useAppDispatch } from './hooks/useRedux'
-import { setAuthLoading, setAuthUser } from './features/auth/authSlice'
-import { supabase } from './lib/supabase'
-import { router } from './routes/AppRouter'
-import { mapAuthUser } from './services/authService'
+import { useEffect } from "react";
+import { RouterProvider } from "react-router-dom";
+import { useAppDispatch } from "./hooks/useRedux";
+import { setAuthLoading, setAuthUser } from "./features/auth/authSlice";
+import { supabase } from "./lib/supabase";
+import { router } from "./routes/AppRouter";
+import { mapAuthUser } from "./services/authService";
 
 const App = () => {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!supabase) {
-      dispatch(setAuthLoading(false))
-      return
+      dispatch(setAuthLoading(false));
+      return;
     }
 
-    dispatch(setAuthLoading(true))
+    dispatch(setAuthLoading(true));
 
     supabase.auth.getSession().then(({ data }) => {
-      const user = data.session?.user
-      dispatch(
-        setAuthUser(
-          user ? mapAuthUser(user) : null,
-        ),
-      )
-      dispatch(setAuthLoading(false))
-    })
+      const user = data.session?.user;
+      dispatch(setAuthUser(user ? mapAuthUser(user) : null));
+      dispatch(setAuthLoading(false));
+    });
 
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
-      const user = session?.user
-      dispatch(
-        setAuthUser(
-          user ? mapAuthUser(user) : null,
-        ),
-      )
-      dispatch(setAuthLoading(false))
-    })
+      const user = session?.user;
+      dispatch(setAuthUser(user ? mapAuthUser(user) : null));
+      dispatch(setAuthLoading(false));
+    });
 
     return () => {
-      subscription.unsubscribe()
-    }
-  }, [dispatch])
+      subscription.unsubscribe();
+    };
+  }, [dispatch]);
 
-  return <RouterProvider router={router} />
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
